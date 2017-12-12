9631c0ec06e2a4c594235a495b663d84fef81966
Yasmin Sahraoui <yasminsahraoui@Yasmins-MacBook-Pro.local>
2017-12-12 12:57:03 +0000

    added few classes and model + modelview for tracking data for main fragment

--- app/src/main/java/com/example/devpair5/trainingapp2/MainFragmentViewModel.java
+++ app/src/main/java/com/example/devpair5/trainingapp2/MainFragmentViewModel.java
@@ -1,9 +1,30 @@
 package com.example.devpair5.trainingapp2;
 
+import java.sql.Time;
+
 /**
  * Created by yasminsahraoui on 08/12/2017.
  */
 
 public class MainFragmentViewModel {
+
+    private Time duration;
+    private int distance;
+    private int calories;
+    private int heartRate;
+
+    public MainFragmentViewModel(){
+        //Time time = new Time();
+    }
+
+    public int getDistance(){
+        return distance;
+    }
+
+    public int getCalories(){
+        return calories;
+    }
+
+    public
 
 }
